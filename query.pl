76ea20174f43f4d01ed526827c640a14d021fdeb
Loic Dachary <loic@dachary.org>
2011-09-14 15:18:06 +0200

    Currently Version unspecified is missing in the picker. But this "version" is required for several bug types what are not inevitably related to LibO versions (WWW, Extensions, Linguistic), so this "Version" unspecified has to be introduced. -- Rainer Bielefeld 2011-09-10T19:35:42 (CEST)

--- query.pl
+++ query.pl
@@ -17,7 +17,8 @@ print "<?xml version='1.0' encoding='ISO-8859-1'?>\n";
 if($ARGV[0] eq 'versions') {
     @versions = @{$vers[$libreoffice]};
     print "<select name='version' class='versions'>\n";
-    print " <option value='?'>(chose a version)</option>\n";
+    print " <option value=''>(chose a version)</option>\n";
+    print " <option value='?'>(Version unspecified)</option>\n";
     for($count = 0; $count < @versions; $count++) {
         if($versions[$count] ne 'unspecified') {
             print " <option value='$versions[$count]'>$versions[$count]</option>\n";
