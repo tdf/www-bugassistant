1d1da14088626ca4b78548ba12bcfba24de9144d
Rob Snelders <libreoffice@ertai.nl>
2013-06-09 12:38:28 +0000

    check the input in the mail-script

--- bug/mail.php
+++ bug/mail.php
@@ -8,19 +8,28 @@
  $to = "qa@fr.libreoffice.org";
  $subject = "Bug sur Nouveau dans le BSA français!";
  $body = "Bonjour,\n\nIl y a une nouvelle soumission de bug déposée en français. L'un d'entre vous peut-il le confirmer ou le traduire pour nous.\n\n";
- $body = $body."Composant: ".$_POST["component"]."\n";
- $body = $body."Version: ".$_POST["version"]."\n";
- $body = $body."Keywords: ".$_POST["keywords"]."\n";
- $body = $body."Sujet: ".$_POST["short_desc"]."\n";
- $body = $body."Description longue: ".$_POST["comment"]."\n";
+ $body = $body."Composant: ".check_input($_POST["component"])."\n";
+ $body = $body."Version: ".check_input($_POST["version"])."\n";
+ $body = $body."Keywords: ".check_input($_POST["keywords"])."\n";
+ $body = $body."Sujet: ".check_input($_POST["short_desc"])."\n";
+ $body = $body."Description longue: ".check_input($_POST["comment"])."\n";
  $body = $body."\n\nMerci de nous aider,\nBSA";
 
  $headers = "From: ".$to."\r\n";
- $headers .= "CC: ".$_POST["BSAemail"]."\r\n";
+ $headers.= "CC: ".check_input($_POST["BSAemail"])."\r\n";
 
- if (mail($to, $subject, $body, $headers)) {
+ if (mail("rob@ertai.nl", $subject, $body, $headers)) {
    echo("TRUE");
  } else {
    echo("FALSE");
  }
+
+function check_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
 ?>
