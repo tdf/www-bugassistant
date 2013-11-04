b6a48c3b03e0d05ac8b792d1deeb00c774ea4a8d
Rob Snelders <programming@ertai.nl>
2013-11-05 00:40:11 +0100

    Improved parameter-structure

--- xmlrpc/xmlrpc.js
+++ xmlrpc/xmlrpc.js
@@ -1,5 +1,5 @@
 function callCalculator() {
-  var request = new XmlRpcRequest("https://bugassistant.libreoffice.org/xmlrpc.cgi", "Bug.search");
+  /*var request = new XmlRpcRequest("https://bugassistant.libreoffice.org/xmlrpc.cgi", "Bug.search");
   request.addParam({id:71220});
   request.crossDomain = true;
   var response = request.send().parseXML();
@@ -9,6 +9,15 @@ function callCalculator() {
   request.addParam({ids:[71220,71196]});
   request.crossDomain = true;
   var response = request.send().parseXML();
-  alert(response.bugs[1].summary);
+  alert(response.bugs[1].summary);*/
+  var count = countBugsOnWhiteboard("Need_Advice");
+  alert(count);
 }
 
+function countBugsOnWhiteboard(text) {
+  var request = new XmlRpcRequest("https://bugassistant.libreoffice.org/xmlrpc.cgi", "Bug.search");
+  request.addParam({whiteboard:text, status:["UNCONFIRMED","NEW","ASSIGNED","REOPENED"], product:"LibreOffice"});
+  request.crossDomain = true;
+  var response = request.send().parseXML();
+  return response.bugs.length;
+}
