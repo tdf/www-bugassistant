eb55a3733c9859c61e8eaf33fa2f120789fa8b01
Rob Snelders <programming@ertai.nl>
2013-11-04 23:26:07 +0100

    Got parameters working now.

--- xmlrpc/xmlrpc.js
+++ xmlrpc/xmlrpc.js
@@ -1,6 +1,14 @@
 function callCalculator() {
-  var request = new XmlRpcRequest("https://bugassistant.libreoffice.org/xmlrpc.cgi", "Bugzilla.version");
+  var request = new XmlRpcRequest("https://bugassistant.libreoffice.org/xmlrpc.cgi", "Bug.search");
+  request.addParam({id:71220});
   request.crossDomain = true;
   var response = request.send().parseXML();
-  alert(response.version);
+  alert(response.bugs[0].priority);
+
+  var request = new XmlRpcRequest("https://bugassistant.libreoffice.org/xmlrpc.cgi", "Bug.get");
+  request.addParam({ids:[71220,71196]});
+  request.crossDomain = true;
+  var response = request.send().parseXML();
+  alert(response.bugs[1].summary);
 }
+
